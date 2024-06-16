5a186cf2eac2f8390febf8bd2b333d0b7e5703d1
MUHAMMAD HAMID RAZA <hamidraza9182@gmail.com>
2024-06-17 04:26:05 +0500

    Add files via upload

---
+++ Lecture 4.py
@@ -0,0 +1,13 @@
+# Exercise = Kilometers to Miles Convertor
+
+# Create a program that asks the user to enter a distance in kilometers, and then converts that distance to miles.and then display its ouput to user.
+
+# 1 Miles = 1.609344 Kilometers
+
+
+
+# solution
+km = float(input("Enter the distance in kilometers:"))
+miles = km/ 1.609344
+
+print("The distance in miles is : ", round(miles,5))
