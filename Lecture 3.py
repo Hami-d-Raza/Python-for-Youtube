d88debc976347103c2953c5e1674b8dc73a3b207
MUHAMMAD HAMID RAZA <hamidraza9182@gmail.com>
2024-06-17 04:25:42 +0500

    Add files via upload

---
+++ Lecture 3.py
@@ -0,0 +1,17 @@
+# number1 = float(input("Enter the first number:"))
+# number2 = float(input("Enter the second number:"))
+
+# print ("The sum of two numbers are: ", number1 + number2 )
+
+
+
+#Data types in python 
+#int = 3,4 ,5 
+# float = 4.67, 6.99
+#string = hamid, age , second number 
+
+
+number1 = input("Enter the first name:")
+number2 = input("Enter the second name:") 
+
+print ("Your full name is " , number1 + " " + number2)
