cbb94acb714c638d7e999972ae5da68cc2403a11
MUHAMMAD HAMID RAZA <hamidraza9182@gmail.com>
2024-06-17 04:25:22 +0500

    Add files via upload

---
+++ Lecture 2.py
@@ -0,0 +1,12 @@
+# Variables in python
+
+number1 = 49
+number2 = 85
+
+print("The sum of two numbers are " , number1 + number2)
+
+
+# lowercase m likhte hain
+# Age     ... print(age)
+# 2age (false)    ... age2 (true)  
+# print   ... Print   
